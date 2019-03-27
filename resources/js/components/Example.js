603b91297ea27040552034329ed72d923c0379a6
mvaughan <magnus.vaughan@gmail.com>
2019-03-27 21:25:37 +0000

    Added fake monarch json

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,18 +1,45 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import jsonResponse from '../../json/monarchs';
 
 export default class Example extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
+    }
+
+    handleClick(e)  {
+        alert('This is being clicked');
+    }
+
     render() {
+
+
+        var monarchList = jsonResponse.map(function(element, index) {
+            return (
+            <div key={index}>
+                <h3>{ element.nm }</h3>
+                <p>{ element.hse }</p>
+            </div>
+            );
+        });
+
+        console.log(monarchList);
+
         return (
             <div className="container">
                 <div className="row justify-content-center">
                     <div className="col-md-8">
                         <div className="card">
-                            <div className="card-header">Example Component</div>
+                            <div onClick={this.handleClick} className="card-header">Larareact</div>
 
                             <div className="card-body">
-                                I'm an example component!
+                                { this.state.date.toLocaleTimeString() }
                             </div>
+
+                            { monarchList }
+
                         </div>
                     </div>
                 </div>
