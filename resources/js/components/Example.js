92721a55eb4838fd72f6158b9f4febf1dd92c7b9
mvaughan <magnus.vaughan@gmail.com>
2019-03-28 08:38:04 +0000

    Looking at basic taskman to see how Laravel backend interacts with the frontend

--- resources/js/components/Example.js
+++ resources/js/components/Example.js
@@ -1,45 +1,38 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import Axios from 'axios';
 import jsonResponse from '../../json/monarchs';
 
 export default class Example extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {date: new Date()};
+        this.state = {
+            date: new Date(),
+            image: ''
+        };
     }
 
     handleClick(e)  {
         alert('This is being clicked');
     }
 
+    componentDidMount() {
+
+    }
+
     render() {
-
-
-        var monarchList = jsonResponse.map(function(element, index) {
-            return (
-            <div key={index}>
-                <h3>{ element.nm }</h3>
-                <p>{ element.hse }</p>
-            </div>
-            );
-        });
-
-        console.log(monarchList);
 
         return (
             <div className="container">
                 <div className="row justify-content-center">
-                    <div className="col-md-8">
+                    <div className="col-md-12">
                         <div className="card">
                             <div onClick={this.handleClick} className="card-header">Larareact</div>
 
                             <div className="card-body">
                                 { this.state.date.toLocaleTimeString() }
                             </div>
-
-                            { monarchList }
-
                         </div>
                     </div>
                 </div>
